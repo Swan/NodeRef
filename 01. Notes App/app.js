c2c75e266be849235a1b9e3cea0a5a6678b85f83
Swan <swan@reborn.com>
2017-03-25 18:07:17 -0400

    Using 3rd Party Modules

--- 01. Notes App/app.js
+++ 01. Notes App/app.js
@@ -2,10 +2,16 @@ console.log("Starting app.js");
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes =  require('./notes.js');
 
-console.log("Result:", notes.add(2, 5));
+console.log(_.isString(true));
+console.log(_.isString('Andrew'));
 
+let filteredArray = _.uniq(['Swan', 1, 2, 'Swan', 3, 9, 12, 'Swan']);
+console.log(filteredArray);
+
+// console.log("Result:", notes.add(2, 5));
 
 // let user = os.userInfo();
 
