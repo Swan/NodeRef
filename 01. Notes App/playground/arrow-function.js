6dc101ca3d4e462974f66eafa77a604ff4712dc8
Swan <swan@reborn.com>
2017-03-26 19:24:08 -0400

    Arrow Functions

---
+++ 01. Notes App/playground/arrow-function.js
@@ -0,0 +1,18 @@
+let square = x => x * x;
+console.log(square(9));
+
+let user = {
+    name: 'Swan',
+    sayHi: () => {
+        console.log(arguments);
+        console.log(`Hi. I'm ${this.name}`);
+    },
+    sayHiAlt () {
+        console.log(arguments);
+        console.log(`Hi. I'm ${this.name}`);
+    }
+};
+
+user.sayHi(1, 2, 3);
+user.sayHiAlt(1, 2, 3);
+
