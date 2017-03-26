cf72ecff959f769e1222242f44d5f8647f0c3bb7
Swan <swan@reborn.com>
2017-03-26 19:52:16 -0400

    Async Basics

---
+++ 02. Weather App/playground/async-basics.js
@@ -0,0 +1,10 @@
+console.log('Startng App');
+
+setTimeout(() => {
+    console.log("Inside of Callback");
+}, 2000);
+
+setTimeout(() => {
+    console.log("Lol");
+}, 0);
+console.log('Finishing App');
