76803ca8e4750e62db6432f0b6bbd7bcc6163d81
Swan <swan@reborn.com>
2017-03-29 14:59:24 -0400

    The ObjectId

--- 05. Node Todo Api/playground/mongodb-connect.js
+++ 05. Node Todo Api/playground/mongodb-connect.js
@@ -1,4 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+let obj = new ObjectID();
+console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     (err) ? console.log('Unable to connect to MongoDB server') : console.log('Connected to MongoDB server');
@@ -11,13 +15,14 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     // });
 
     // Insert new doc into users collection (name, age, location)
-    db.collection('Users').insertOne({
-        name: "Swan",
-        age: 19,
-        location: "The Pond"
-    }, (err, result) => {
-        (err) ? console.log('Unable to insert user', err) : console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    // db.collection('Users').insertOne({
+
+    //     name: "peppy",
+    //     age: 19,
+    //     location: "Australia"
+    // }, (err, result) => {
+    //     (err) ? console.log('Unable to insert user', err) : console.log(result.ops[0]._id.getTimestamp());
+    // });
 
     db.close();
 });
