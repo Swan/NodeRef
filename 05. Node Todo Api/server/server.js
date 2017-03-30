d7055b528e15223b247fc7f0a6075a88ed550218
Swan <swan@reborn.com>
2017-03-30 17:20:03 -0400

    Setting Up The User Model

--- 05. Node Todo Api/server/server.js
+++ 05. Node Todo Api/server/server.js
@@ -96,6 +96,21 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
+
+// POST /users
+app.post('/users', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  let user = new User(body);
+
+  user.save()
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    })
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
