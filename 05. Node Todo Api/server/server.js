435d4fedc370c407d409910bb2824bd0164c6c6b
Swan <swan@reborn.com>
2017-03-30 16:14:34 -0400

    Add GET /todos route

--- 05. Node Todo Api/server/server.js
+++ 05. Node Todo Api/server/server.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
+
 app.post('/todos', (req, res) => {
     let todo = new Todo({
         text: req.body.text
@@ -24,6 +25,18 @@ app.post('/todos', (req, res) => {
             res.status(400).send(err);
         });
 });
+
+
+app.get('/todos', (req, res) => {
+    Todo.find()
+        .then((todos) => {
+            res.send({todos})
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
+});
+
 
 app.listen(3000, () => {
     console.log('Server started on port: 3000');
