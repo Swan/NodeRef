8bd114301a375e4c0b491a0a2910385034f4544f
Swan <swan@reborn.com>
2017-03-30 16:33:23 -0400

    Mongoose Queries & ID Validation

--- 05. Node Todo Api/playground/mongoose-queries.js
+++ 05. Node Todo Api/playground/mongoose-queries.js
@@ -3,31 +3,31 @@ const {ObjectID} = require('mongodb');
 const mongoose = require('./../server/db/mongoose');
 const Todo = require('./../server/models/todo'); 
 
-// let id = '58dc1499ca4d5514c06f7b74';
+let id = '58dc1499ca4d5514c06f7b74';
 
-// if (!ObjectID.isValid(id)) {
-//     console.log('ID not valid');
-// }
+if (!ObjectID.isValid(id)) {
+    console.log('ID not valid');
+}
 
-// Todo.find({
-//     _id: id
-// }).then((todos) => {
-//     console.log('Todos', todos);
-// });
+Todo.find({
+    _id: id
+}).then((todos) => {
+    console.log('Todos', todos);
+});
 
-// Todo.findOne({
-//     _id: id
-// }).then((todo) => {
-//     console.log('Todo', todo);
-// })
+Todo.findOne({
+    _id: id
+}).then((todo) => {
+    console.log('Todo', todo);
+})
 
-// Todo.findById(id)
-//     .then((todo) => {
-//         if(!todo) {
-//             return console.log('Id not found.');
-//         }
-//         console.log('Todo by Id:', todo);
-//     })
-//     .catch((err) => {
-//         console.log('Id not valid')
-//     });
+Todo.findById(id)
+    .then((todo) => {
+        if(!todo) {
+            return console.log('Id not found.');
+        }
+        console.log('Todo by Id:', todo);
+    })
+    .catch((err) => {
+        console.log('Id not valid')
+    });
