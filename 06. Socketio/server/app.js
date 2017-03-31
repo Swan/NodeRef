63a63ff6870bcaba3e735252da50983c55a5caf5
Swan <swan@reborn.com>
2017-03-31 15:21:48 -0400

    Emit newMessasge on createMessage

--- 06. Socketio/server/app.js
+++ 06. Socketio/server/app.js
@@ -13,28 +13,15 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
+
     console.log('New user connected');
 
-    // emit -- sending from server to client
-    socket.emit('newEmail', {
-        from: 'the@swan.moe',
-        text: 'Hey. What\'s up?!',
-        createdAt: 123
-    });
-
-    socket.emit('newMessage', {
-        from: 'AiAe',
-        text: '(╯°□°）╯︵ ┻━┻', 
-        createdAt: 03312017
-    });
-
-    // socket.on -- listening to received messages from the client
-    socket.on('createEmail',  (newEmail) => {
-        console.log('createEmail:', newEmail);
-    });
-
-    socket.on('createMessage', (newMessage) => {
-        console.log('New message sent from the client', newMessage);
+    socket.on('createMessage', (message) => {
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
