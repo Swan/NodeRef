54d007e81e2431d4779dc17af914e626cd6f6fc7
Swan <swan@reborn.com>
2017-04-03 14:27:36 -0400

    Add: Comments for Hello World

--- hello-world.js
+++ hello-world.js
@@ -1,3 +1,6 @@
+/*
+ * The most basic of programs you could make in Node.js. The "Hello, World."
+ * You can run this by running `node hello-world.js`
+ */
 console.log("Hello, World!");
 
-// To start, run node app.js in the terminal
