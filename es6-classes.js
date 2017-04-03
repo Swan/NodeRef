45a4078be5aef7c1c2982889216d5903f3d1996f
Swan <swan@reborn.com>
2017-04-03 14:21:59 -0400

    Add: Comments & Resources for ES6 Classes

--- es6-classes.js
+++ es6-classes.js
@@ -1,18 +1,43 @@
+/*
+ * You can read more about ES6 Classes here:
+ *  Resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+ */ 
+
 class Person {
-    // constructor -- called automatically upon instantiation
+    /*
+     * Constructor function is called immediately upon instantion. Useful if you want to set any values.
+     */
     constructor (name, age) {
-        // "this" is how you customize the individual instance
+        /*
+         * the 'this' keywords is especially useful for customizing/personalizing an individual instance.
+         * As you can see here, I'm setting this.name equal to the name that was passed in, and same for the age.    
+         */ 
         this.name = name;
         this.age = age;
     }
 
+    /*
+     * You can also define functions in a class. In this case, we're just returning a description of the person.
+     */
     getUserDescription () {
         return `${this.name} is ${this.age} year(s) old.`;
     }
 }
 
+/*
+ * Here's how you can instantiate a new person, passing in "Swan" for the name, and 19 for the age.
+ */ 
 let me = new Person('Swan', 19);
+
+/*
+ * You can now use the individual values from the declared variable "me."
+ * me.name == this.name
+ */
 console.log('this.name', me.name);
-console.log('this.age', me.age);
-let description = me.getUserDescription();
-console.log(description);
+
+
+/*
+ * You can also call functions on that class, like so. In this case, getUserDescription returns a description
+ * about the individual Person.
+ */
+console.log(me.getUserDescription());
