1cb0023335d8267c5dc7ca20439333dd51c6fab8
Swan <swan@reborn.com>
2017-04-03 13:46:27 -0400

    Add: Comments & Resources for debugging Node.js applications

--- debugging.js
+++ debugging.js
@@ -1,9 +1,19 @@
+/*
+ *  You can use the Node.js debugger to debug your code in the command line. 
+ *  Running `node debug filename.js` will allow you to run the debugger.
+ *  You can learn more about Debugging here:
+ *      Resource: https://nodejs.org/api/debugger.html
+ */
+
 let person = {
     name: "Swan"
 };
 
 person.age = 25;
 
+/*
+ * Calling the debugger; statement will allow you to skip exactly where you want to start debugging.
+ */ 
 debugger;
 
 person.name = 'Mike';
