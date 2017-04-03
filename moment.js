1f8c77b97e25a0fc79f4a3afc1a462dab6ff556b
Swan <swan@reborn.com>
2017-04-03 14:36:38 -0400

    Add: Comments & Resources for Moment.js

--- moment.js
+++ moment.js
@@ -1,11 +1,29 @@
-// Jan 1st 1970 00:00:00 am
+/*
+ * Moment.js is a Node module that helps EXTREMELY with dates.
+ * Provides an easy way to format dates, add to dates, subtract, etc.
+ * You can read more about Moment.js here: https://momentjs.com/
+ */
+
+/*
+ * Loading in the module.
+ */
 const moment = require('moment');
 
-// let date = new Date();
-// console.log(date.getMonth());
+/*
+ * Creating a timestamp in module.
+ * Very similar to calling new Date()
+ */
 let someTimestamp = moment().valueOf();
 console.log(someTimestamp);
 
+
+/*
+ * Example of creating a new date, from Unix Epoch time, and running operations on it.*
+ */
 let date = moment(1234);
 date.add(1, 'year').subtract(9, 'months');
+
+/*
+ * Formatting dates, read the docs for Moment.js to find out more about this
+ */
 console.log(date.format('MMM Do, YYYY'));
