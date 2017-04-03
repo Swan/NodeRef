619bbfc9159bb29a11ca12235ba9c7096c74ab13
Swan <swan@reborn.com>
2017-04-03 13:56:59 -0400

    Add: Comments & Resources for ES6 Arrow Functions

--- es6-arrow-functions.js
+++ es6-arrow-functions.js
@@ -1,18 +1,48 @@
-let square = x => x * x;
-console.log(square(9));
+/*
+ * Arrow functions is a feature of ECMAScript 6 that allows us to create functions faster with shorter syntex.
+ * To learn more about arrow functions, you can look here:
+ *  Resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+ */ 
 
+
+/*
+ * Here is an example of an arrow function, where (num1, num2) are the arguments. 
+ * () => is the shorthand syntax for defining a function.
+ */
+let addNumbers = (num1, num2) => {
+    return num1 + num2
+};
+
+/*
+ * However, if you only have one argument, and one return statement, it becomes even shorter
+ * where x => is the number passed in, and x * x; is what's being returned.
+ */
+let squareNumbers = x => x * x;
+
+
+/*
+ * There are some limitations to arrow functions, however, as seen below. 
+ */
 let user = {
     name: 'Swan',
-    sayHi: () => {
-        console.log(arguments);
+
+    /*
+     * You cannot use arrow functions in an object. Try running it.
+     */
+    sayHiGlobal: (num1, num2) => {
+        console.log(num1 + num2);
         console.log(`Hi. I'm ${this.name}`);
     },
+
+    /*
+     * Instead, here's the proper syntax you should use.
+     */ 
     sayHiAlt () {
         console.log(arguments);
         console.log(`Hi. I'm ${this.name}`);
     }
 };
 
-user.sayHi(1, 2, 3);
+user.sayHiGlobal(1, 2);
 user.sayHiAlt(1, 2, 3);
 
