c02d61060a07c93d0d73db4bf4b409eb06b60c24
Swan <swan@reborn.com>
2017-04-03 15:08:43 -0400

    Add: Comments & Resources to Hashing

--- hashing.js
+++ hashing.js
@@ -1,28 +1,56 @@
+/*
+ * Hashing - One-way cryptography. Useful for storing passwords or checksums
+ * Read More about hashing and the crypto-js module here: 
+ *  Resource: https://www.npmjs.com/package/crypto-js
+ */
 const {SHA256} = require('crypto-js');
 
-// let message = 'I am user number 3';
-// let hash = SHA256(message).toString();
+/*
+ * Hashing the "messagge" in SHA256
+ */ 
+const SALT = 'some_random_string_you_should_not_tell_anyone_about'
+let message = 'I am user number 3';
+let hash = SHA256(message + SALT).toString();
 
-// console.log(`Message: ${message}`);
-// console.log(`Hash: ${hash}`);
+console.log(`Message: ${message}`);
+console.log(`Hash: ${hash}`);
 
-// let data = {
-//     id: 4
-// };
 
-// let token = {
-//     data, 
-//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// };
+let data = {
+    id: 4
+};
 
-// // token.data.id = 5;
-// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+let token = {
+    data, 
+    hash: SHA256(JSON.stringify(data) + SALT).toString()
+};
 
-// let resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
 
-// if (resultHash === token.hash) {
-//     console.log('Data was not changed');
-// } else {
-//     console.log('Data was changed. Dont trust');
-// }
+/*
+ * Even if the data was changed on the client side, the user doesn't have access to the secret, so it'll fail as well.
+ * They can change the data client side, but if they don't have the secret, we can easily tell, because it won't match.
+ */
+
+// Failing Case (Data Id was changed)
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data + SALT)).toString();
+
+// Passing Case (Data was not changed)
+token.data.id = 4;
+token.hash = SHA256(JSON.stringify(token.data + SALT)).toString();
+console.log('Token Hash:', token.hash);
+
+
+let resultHash = SHA256(JSON.stringify(token.data + SALT)).toString();
+console.log('ResultHash:', resultHash)
+
+
+/*
+ * Comparing both hashes
+ */
+if (resultHash === token.hash) {
+    console.log('Data was not changed');
+} else {
+    console.log('Data was changed. Dont trust');
+}
 
