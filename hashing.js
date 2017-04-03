4e6303302ad768dd698334d4c7e5bfaf5a713063
Swan <swan@reborn.com>
2017-04-03 14:55:42 -0400

    Create and Move: JWT into its own file

--- hashing.js
+++ hashing.js
@@ -1,15 +1,4 @@
 const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
-
-let data = {
-    id: 10
-};
-
-let token = jwt.sign(data, '123abc');
-console.log(token);
-
-let decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
 
 // let message = 'I am user number 3';
 // let hash = SHA256(message).toString();
